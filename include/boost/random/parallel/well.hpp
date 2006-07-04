d3583ff1d95a3cb579dc73352c24da52a545e927
Matthias Troyer <troyer@phys.ethz.ch>
2006-07-04 08:16:31 +0000

    Addded copyright
    [SVN r3053]

--- include/boost/random/parallel/well.hpp
+++ include/boost/random/parallel/well.hpp
@@ -1,3 +1,12 @@
+/* 
+ * Copyright Brigitte Surer and Matthias Troyer 2006
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+*
+ */
+
+
 #ifndef BOOST_RANDOM_PARALLEL_WELL_HPP
 #define BOOST_RANDOM_PARALLEL_WELL_HPP
 
