9fa0b198b5c389b3fd278db55cb630206307d602
Matt Borland <matt@mattborland.com>
2022-11-29 20:55:17 -0800

    Update tests to match c++11 requirements

--- test/test_splitmix64.cpp
+++ test/test_splitmix64.cpp
@@ -1,72 +1,23 @@
-/*
- * Copyright (c) 2010 Steven Watanabe
- * Copyright (c) 2022 Matt Borland
+/* test_splitmix64.cpp
+ *
+ * Copyright Steven Watanabe 2011
+ * Copyright Matt Borland 2022
  * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE_1_0.txt or copy at
- * http://www.boost.org/LICENCE_1_0.txt)
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * $Id$
  *
  */
 
-#include <cstdint>
-#include <array>
-#include <limits>
-#include <random>
-#include <boost/core/lightweight_test.hpp>
-#include <boost/math/statistics/univariate_statistics.hpp>
 #include <boost/random/splitmix64.hpp>
+#include <cstdint>
+#include <cmath>
 
-int main(void)
-{
-    // Test the values are in the specified min and max range
-    constexpr auto const_min = (std::numeric_limits<std::uint64_t>::min)();
-    constexpr auto const_max = (std::numeric_limits<std::uint64_t>::max)();
-    boost::random::splitmix64 rng;
-    for(int i = 0; i < 1000; ++i) 
-    {
-        std::uint64_t val = rng();
-        BOOST_TEST_GE(val, const_min);
-        BOOST_TEST_LE(val, const_max);
-    }
+#define BOOST_RANDOM_URNG boost::random::splitmix64
 
-    // Test the generate function
-    std::array<std::uint64_t, 1000> test_array {};
-    rng.generate(test_array.begin(), test_array.end());
+// principal operation validated with CLHEP, values by experiment
+#define BOOST_RANDOM_VALIDATION_VALUE UINT64_C(542758903869407163)
+#define BOOST_RANDOM_SEED_SEQ_VALIDATION_VALUE UINT64_C(7044339178176046395)
 
-    // Value validation after 10000 uses with specified seed a la STL
-    // Seed = 42
-    // N = 10000
-    // Validation value = 0x62ed6a69aa8c7b8d
-    boost::random::splitmix64 validation {UINT64_C(42)};
-    for(int i = 0; i < 9999; ++i)
-    {
-        validation();
-    }
-
-    const std::uint64_t validation_value = validation();
-    BOOST_TEST_EQ(validation_value, UINT64_C(0x62ed6a69aa8c7b8d));
-
-    // Test use in a standard distribution
-    // Mean of uniform int distribution should be 5 given (1, 9)
-    std::uniform_int_distribution<std::uint64_t> distrib(1,9);
-    std::array<std::uint64_t, 1000> values {};
-    for(std::size_t i = 0; i < 1000; ++i)
-    {
-        values[i] = distrib(rng);
-    }
-
-    const auto dist_mean = boost::math::statistics::mean(values);
-    BOOST_TEST_GE(dist_mean, 4.5);
-    BOOST_TEST_LE(dist_mean, 5.5);
-
-    // Test that two default constructed generators do not have the same state
-    boost::random::splitmix64 gen1;
-    boost::random::splitmix64 gen2;
-    BOOST_TEST_NE(gen1(), gen2());
-
-    // Test that two specifed generators do have the same state
-    boost::random::splitmix64 gen3 {UINT64_C(42)};
-    boost::random::splitmix64 gen4 {UINT64_C(42)};
-    BOOST_TEST_EQ(gen3(), gen4());
-
-    return boost::report_errors();
-}
+#include "test_generator.ipp"
