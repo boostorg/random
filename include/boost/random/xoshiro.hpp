b35c9c12a13b12653100cd0fc64020d747948679
Matt Borland <matt@mattborland.com>
2022-12-10 14:54:08 -0800

    Add xoshiro256**

--- include/boost/random/xoshiro.hpp
+++ include/boost/random/xoshiro.hpp
@@ -157,6 +157,58 @@ public:
     }
 };
 
+/* This is xoshiro256** 1.0, one of our all-purpose, rock-solid
+ * generators. It has excellent (sub-ns) speed, a state (256 bits) that is
+ * large enough for any parallel application, and it passes all tests we
+ * are aware of.
+ *
+ * For generating just floating-point numbers, xoshiro256+ is even faster.
+ */
+class xoshiro256_starstar final : public detail::xoshiro<4>
+{
+public:
+    // Use all of the base classes constructors 
+    using detail::xoshiro<4>::xoshiro;
+    
+    inline result_type next() noexcept override
+    {
+        const std::uint64_t result = rotl(state_[1] * 5, 7) * 9;
+        const std::uint64_t t = state_[1] << 17;
+
+        state_[2] ^= state_[0];
+        state_[3] ^= state_[1];
+        state_[1] ^= state_[2];
+        state_[0] ^= state_[3];
+
+        state_[2] ^= t;
+
+        state_[3] = rotl(state_[3], 45);
+
+        return result;
+    }
+
+    // This is the jump function for the generator. It is equivalent
+    // to 2^128 calls to next(); it can be used to generate 2^128
+    // non-overlapping subsequences for parallel computations.
+    inline void jump() noexcept override
+    {
+        static constexpr std::array<std::uint64_t, 4> jump_pos {0x180EC6D33CFD0ABA, 0xD5A61266F0C9392C, 
+                                                                0xA9582618E03FC9AA, 0x39ABDC4529B1661C};
+        state_ = detail::jump_impl(this, jump_pos);
+    }
+
+    // This is the long-jump function for the generator. It is equivalent to
+    // 2^192 calls to next(); it can be used to generate 2^64 starting points,
+    // from each of which jump() will generate 2^64 non-overlapping
+    // subsequences for parallel distributed computations.
+    inline void long_jump() noexcept override
+    {
+        static constexpr std::array<std::uint64_t, 4> long_jump_pos {0x76E15D3EFEFDCBBF, 0xC5004E441C522FB3, 
+                                                                     0x77710069854EE241, 0x39109BB02ACBE635};
+        state_ = jump_impl(this, long_jump_pos);
+    }
+};
+
 }} // Namespace boost::random
 
 #endif // BOOST_RANDOM_XOSHIRO_HPP
