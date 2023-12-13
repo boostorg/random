53d1774df4a09b2c099d89e56fcbf6e62d95de14
Matt Borland <matt@mattborland.com>
2023-12-13 12:04:23 +0100

    Address review comments

--- include/boost/random/splitmix64.hpp
+++ include/boost/random/splitmix64.hpp
@@ -5,6 +5,22 @@
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  * 
+ */
+
+#ifndef BOOST_RANDOM_SPLITMIX64_HPP
+#define BOOST_RANDOM_SPLITMIX64_HPP
+
+#include <cstdint>
+#include <cstdlib>
+#include <limits>
+#include <array>
+#include <string>
+#include <ios>
+#include <type_traits>
+
+namespace boost { namespace random {
+
+/**
  *  This is a fixed-increment version of Java 8's SplittableRandom generator
  *  See http://dx.doi.org/10.1145/2714064.2660195 and
  *  http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
@@ -14,22 +30,6 @@
  *  computations) or xorshift1024* (for massively parallel computations)
  *  generator.
  */
-
-#ifndef BOOST_RANDOM_SPLITMIX64_HPP
-#define BOOST_RANDOM_SPLITMIX64_HPP
-
-#include <cstdint>
-#include <cstdlib>
-#include <limits>
-#include <random>
-#include <array>
-#include <string>
-#include <ios>
-#include <iostream>
-#include <type_traits>
-
-namespace boost { namespace random {
-
 class splitmix64
 {
 private:
@@ -111,12 +111,7 @@ public:
 
     inline friend bool operator==(const splitmix64& lhs, const splitmix64& rhs) noexcept
     {
-        if (lhs.state_ == rhs.state_)
-        {
-            return true;
-        }
-
-        return false;
+        return lhs.state_ == rhs.state_;
     }
 
     inline friend bool operator!=(const splitmix64& lhs, const splitmix64& rhs) noexcept
@@ -128,25 +123,22 @@ public:
     inline friend std::basic_ostream<CharT,Traits>& operator<<(std::basic_ostream<CharT,Traits>& ost, 
                                                                const splitmix64& e)
     {
-        std::string sstate = std::to_string(e.state_);
-        for (const auto i : sstate)
-        {
-            ost << i;
-        }
-
+        ost << e.state_;
         return ost;
     }
 
     template <typename CharT, typename Traits>
-    friend std::basic_istream<CharT,Traits>& operator>>(std::basic_istream<CharT,Traits>& ist, 
-                                                        splitmix64& e)
+    inline friend std::basic_istream<CharT,Traits>& operator>>(std::basic_istream<CharT,Traits>& ist,
+                                                               splitmix64& e)
     {
         std::string sstate;
-        for (std::size_t i {}; i <= std::numeric_limits<std::uint64_t>::digits10; ++i)
+        CharT val;
+        while (ist >> val)
         {
-            CharT val;
-            ist >> val >> std::ws;
-            sstate.push_back(val);
+            if (std::isdigit(val))
+            {
+                sstate.push_back(val);
+            }
         }
         
         e.state_ = std::strtoull(sstate.c_str(), nullptr, 10);
