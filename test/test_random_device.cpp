8bc4ae8553be60fe2856b1f423e723d244a64cd7
Alexander Grund <Flamefire@users.noreply.github.com>
2026-09-08 10:08:42 +0200

    Port test_random_device to non-"included" Boost.Test

--- test/test_random_device.cpp
+++ test/test_random_device.cpp
@@ -1,6 +1,7 @@
 /* boost random_test.cpp various tests
  *
  * Copyright (c) 2010 Steven Watanabe
+ * Copyright (c) 2016 Alexander Grund
  * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENCE_1_0.txt)
@@ -10,15 +11,16 @@
 
 #include <boost/random/random_device.hpp>
 
-#include <boost/test/test_tools.hpp>
-#include <boost/test/included/test_exec_monitor.hpp>
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
 
 #ifndef BOOST_NO_CXX11_CONSTEXPR
 constexpr boost::random_device::result_type const_min = (boost::random_device::min)();
 constexpr boost::random_device::result_type const_max = (boost::random_device::max)();
 #endif
 
-int test_main(int, char**) {
+BOOST_AUTO_TEST_CASE(test_random_device)
+{
     boost::ignore_unused(const_min);
     boost::ignore_unused(const_max);
 
@@ -33,5 +35,4 @@ int test_main(int, char**) {
 
     boost::uint32_t a[10];
     rng.generate(a, a + 10);
-    return 0;
 }
